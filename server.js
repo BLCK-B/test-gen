466d9ea31a31ef34f68e4fbe9c99d1bd30e3dd34
BLCK <123077751+BLCK-B@users.noreply.github.com>
2026-05-29 07:55:49 +0200

    init

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import cors from 'cors'
 const app = express()
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://gen.online-data.uk'],
+    origin: true,
     methods: ['POST', 'GET', 'OPTIONS'],
     credentials: true
 }))
